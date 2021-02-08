c1c320091ee9169a68bd03767682510633546300
Nick Flueckiger <nick.flueckiger@renuo.ch>
2021-02-08 11:47:02 +0100

    Init

---
+++ lib/rusty/swiss/village/directory.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "directory/version"
+
+module Rusty
+  module Swiss
+    module Village
+      module Directory
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/rusty/swiss/village/directory/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Rusty
+  module Swiss
+    module Village
+      module Directory
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
